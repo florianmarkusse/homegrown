3337b7f3f03855e135664ea53a74a799d7e8ab7d
florianmarkusse <florianmarkusse@hotmail.com>
2024-10-07 21:43:02 +0200

    fix uefiimage.go

--- cmd/common/uefiimage/uefiimage.go
+++ cmd/common/uefiimage/uefiimage.go
@@ -1,19 +1,18 @@
 package uefiimage
 
 import (
-	"cmd/common"
 	"cmd/common/argument"
 	"cmd/common/cmake"
 	"fmt"
 )
 
 func CreateUefiImage(cCompiler string, buildMode string) {
-	copyEfiCommand := fmt.Sprintf("find %s -executable -type f -name \"uefi\" -exec cp {} BOOTX64.EFI \\;", cmake.BuildDirectoryRoot(common.UEFI_CODE_FOLDER, false, cCompiler))
+	copyEfiCommand := fmt.Sprintf("find %s -executable -type f -name \"uefi\" -exec cp {} BOOTX64.EFI \\;", cmake.BuildDirectoryRoot(cmake.PROJECT_STRUCTURES[cmake.UEFI].CodeFolder, false, cCompiler))
 	argument.ExecCommand(copyEfiCommand)
 
-	copyKernelCommand := fmt.Sprintf("find %s -executable -type f -name \"kernel.bin\" -exec cp {} kernel.bin \\;", cmake.BuildDirectoryRoot(common.KERNEL_CODE_FOLDER, false, cCompiler))
+	copyKernelCommand := fmt.Sprintf("find %s -executable -type f -name \"kernel.bin\" -exec cp {} kernel.bin \\;", cmake.BuildDirectoryRoot(cmake.PROJECT_STRUCTURES[cmake.KERNEL].CodeFolder, false, cCompiler))
 	argument.ExecCommand(copyKernelCommand)
 
-	createTestImageCommand := fmt.Sprintf("find %s -type f -name \"uefi-image-creator\" -exec {} --data-size 32 -ae /EFI/BOOT/ BOOTX64.EFI -ad kernel.bin \\;", cmake.BuildDirectoryRoot(common.UEFI_IMAGE_CREATOR_CODE_FOLDER, false, cCompiler))
+	createTestImageCommand := fmt.Sprintf("find %s -type f -name \"uefi-image-creator\" -exec {} --data-size 32 -ae /EFI/BOOT/ BOOTX64.EFI -ad kernel.bin \\;", cmake.BuildDirectoryRoot(cmake.PROJECT_STRUCTURES[cmake.UEFI_IMAGE_CREATOR].CodeFolder, false, cCompiler))
 	argument.ExecCommand(createTestImageCommand)
 }
