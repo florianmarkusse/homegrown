aef527f69f1c342cadbcf9ab36f7ab145c2c3578
florianmarkusse <florianmarkusse@hotmail.com>
2024-12-09 21:11:48 +0100

    somewhat correct cpu abstraction

--- projects/platform-abstraction/code/cpu/include/platform-abstraction/cpu.h
+++ projects/platform-abstraction/code/cpu/include/platform-abstraction/cpu.h
@@ -1,16 +1,12 @@
 #ifndef PLATFORM_ABSTRACTION_CPU_H
 #define PLATFORM_ABSTRACTION_CPU_H
 
-#ifdef X86_ARCHITECTURE
-#ifdef FREESTANDING_ENVIRONMENT
-#include "x86/cpu/real/cpu.h"
-#elif POSIX_ENVIRONMENT
-#include "x86/cpu/mock/cpu.h"
-#else
-#error "Could not match ENVIRONMENT"
-#endif
-#else
-#error "Could not match ARCHITECTURE"
-#endif
+#include "shared/types/types.h"
+
+U64 rdmsr(U32 msr);
+void wrmsr(U32 msr, U64 value);
+
+void flushTLB();
+void flushCPUCaches();
 
 #endif
--- projects/x86/code/cpu/mock/include/x86/cpu/mock/cpu.h
+++ projects/x86/code/cpu/mock/include/x86/cpu/mock/cpu.h
@@ -1,6 +1,12 @@
 #ifndef X86_CPU_MOCK_CPU_H
 #define X86_CPU_MOCK_CPU_H
 
-#include "x86/cpu/real/cpu.h"
+#include "shared/types/types.h"
+
+U64 rdmsr(U32 msr);
+void wrmsr(U32 msr, U64 value);
+
+void flushTLB();
+void flushCPUCaches();
 
 #endif
