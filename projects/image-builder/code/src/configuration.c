f4cd0b4ba272df9b48e568c59f5334140032c939
florianmarkusse <florianmarkusse@hotmail.com>
2024-12-31 23:08:05 +0100

    set default configuration to be equal to uefi-image-creator

--- projects/image-builder/code/src/configuration.c
+++ projects/image-builder/code/src/configuration.c
@@ -1,14 +1,19 @@
 #include "image-builder/configuration.h"
 #include "shared/memory/sizes.h"
+#include <stdlib.h>
+#include <time.h>
 
 // TODO: Move default LBA size to 4096 , seems better for performone on disks in
 // this day and age
 // And have it be dependent on or set it manually
 // static U64 physicalBlockBoundary = 512;
 // static U64 optimalTransferLengthGranularity = 512;
-Configuration configuration = {.imageName = "new-image.hdd", .LBASize = 1024};
+Configuration configuration = {.imageName = "image-builder-image.hdd",
+                               .LBASize = 512};
 
 void setConfiguration() {
+    srand((U32)time(nullptr));
+
     // NOTE: this is just scaffolding, it should calculate the actual values
     // instead of just using hardcoded values.
 
